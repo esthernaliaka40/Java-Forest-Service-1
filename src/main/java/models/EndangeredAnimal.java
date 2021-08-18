9fe03a2cd60f0acba99b03f5c7630cbb33970b89
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-18 22:00:03 +0300

    add change to EndangeredAnimal

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class EndangeredAnimal {
     private String name;
     private int id;
@@ -13,4 +15,48 @@ public class EndangeredAnimal {
         this.age = age;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EndangeredAnimal)) return false;
+        EndangeredAnimal that = (EndangeredAnimal) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(health, that.health) && Objects.equals(age, that.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, health, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
