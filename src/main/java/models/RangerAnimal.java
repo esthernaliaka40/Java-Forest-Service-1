c7e69c5dd616dce92945d2fd10f71cc11467ab4d
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-18 21:44:38 +0300

    add new moodel package and tests

--- src/main/java/models/RangerAnimal.java
+++ src/main/java/models/RangerAnimal.java
@@ -1,5 +1,62 @@
 package models;
 
-public class RangerAnimal {
+import java.util.Objects;
 
+public class RangerAnimal {
+    private String name;
+    private int tag;
+    private int phone;
+    private String email;
+
+    public RangerAnimal(String name, int tag, int phone, String email) {
+        this.name = name;
+        this.tag = tag;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RangerAnimal testRanger = (RangerAnimal) o;
+        return tag == testRanger.tag && phone == testRanger.phone && name.equals(testRanger.name) && email.equals(testRanger.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, tag, phone, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTag() {
+        return tag;
+    }
+
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/test/java/models/RangerAnimalTest.java
+++ src/test/java/models/RangerAnimalTest.java
@@ -2,16 +2,51 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RangerAnimalTest {
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception{
+    }
+
+    @Test
+    void GetInstantiatesObjectsTrueCorrectly() throws Exception{
+        RangerAnimal testRanger = setupRanger();
+        assertTrue(testRanger instanceof RangerAnimal);
+    }
+
+    @Test
+    void GetRangerStringNameAndInstantiateCorrectly() throws Exception{
+        RangerAnimal testRanger = setupRanger();
+        assertEquals("Todd", testRanger.getName());
+    }
+
+    @Test
+    void GetRangerIntTagNumberInstantiatesCorrectly() throws Exception{
+        RangerAnimal testRanger = setupRanger();
+        assertEquals(10, testRanger.getTag());
+    }
+
+    @Test
+    void GetRangersContactNoAndInstantiatesCorrectly_true() {
+        RangerAnimal testRanger = setupRanger();
+        assertEquals(01003, testRanger.getPhone());
+    }
+
+    @Test
+    void GetEmailOfRangerInstantiatesCorrectlyReturns_true() {
+        RangerAnimal testRanger = setupRanger();
+        assertEquals("todd@gmail.com", testRanger.getEmail());
+    }
+
+    public RangerAnimal setupRanger() {
+        return new RangerAnimal("Todd", 10, 01003, "todd@gmail.com");
     }
 }
