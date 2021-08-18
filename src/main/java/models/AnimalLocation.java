8ccd95668aef5be03df8e445f7dfba1734e7a3d4
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-18 21:05:28 +0300

    add new moodel package and tests

--- src/main/java/models/AnimalLocation.java
+++ src/main/java/models/AnimalLocation.java
@@ -1,5 +1,41 @@
 package models;
 
-public class AnimalLocation {
+import java.util.Objects;
 
+public class AnimalLocation {
+  private String name;
+  private int id;
+
+    public AnimalLocation(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnimalLocation testLocation = (AnimalLocation) o;
+        return id == testLocation.id && name.equals(testLocation.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/test/java/models/AnimalLocationTest.java
+++ src/test/java/models/AnimalLocationTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,27 @@ class AnimalLocationTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void GetsLocationInstantiatesCorrectly() {
+        AnimalLocation testLocation = setupLocation();
+        assertTrue(testLocation instanceof AnimalLocation);
+    }
+
+    @Test
+    void GetsStringNameOfLocationCorrectly() {
+        AnimalLocation testLocation = setupLocation();
+        assertEquals("Zone A", testLocation.getName());
+    }
+
+    @Test
+    void GetIdOfAreaOfLocation() {
+        AnimalLocation testLocation = setupLocation();
+        assertEquals(0, testLocation.getId());
+    }
+
+    public AnimalLocation setupLocation() {
+        return new AnimalLocation("Zone A");
     }
 }
