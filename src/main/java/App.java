968040010addf1a7615639fabe39b40a2a041311
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-19 02:27:34 +0300

    add new directories and changes in hbs files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import  java.util.Map;
 import java.util.HashMap;
 import java.util.Objects;
 
+import models.RangerAnimal;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -15,5 +16,31 @@ public class App {
             Map<String,Object> model = new HashMap<>();
             return new ModelAndView(model, "layout.hbs");
         }, new HandlebarsTemplateEngine());
+
+
+        get("/about", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "about.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //Ranger
+        //Create A ranger
+        get("wildlifeRanger/new", (request, response) -> {
+           Map<String, Object> model = new HashMap<String , Object>();
+           return new ModelAndView(model, "Wildliferangerform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //Post a new Ranger
+        post("/wildlifeRanger", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("rangerName");
+            int tag = Integer.parseInt(request.queryParams("rangerTag"));
+            int phone = Integer.parseInt(request.queryParams("rangerPhone"));
+            String email = request.queryParams("rangerEmail");
+            RangerAnimal testRanger = new RangerAnimal(name, tag, phone, email);
+            model.put("ranger", testRanger);
+            return new ModelAndView(model, "Wildliferangerform.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
