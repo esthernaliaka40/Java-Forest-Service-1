82ce4a6892c46931e60d55a6351ae50413394ce0
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-18 21:24:06 +0300

    add new moodel package and tests

--- src/main/java/models/AnimalSighting.java
+++ src/main/java/models/AnimalSighting.java
@@ -1,5 +1,82 @@
 package models;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
+
 public class AnimalSighting {
 
+    private int id;
+    private int animalId;
+    private int rangerId;
+    private int locationId;
+    private Date date = new Date();
+    private Timestamp time;
+
+    public AnimalSighting(int animalId,  int rangerId, int locationId) {
+        this.animalId = animalId;
+        this.rangerId = rangerId;
+        this.locationId =locationId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnimalSighting testSighting = (AnimalSighting) o;
+        return id == testSighting.id && rangerId == testSighting.rangerId && animalId == testSighting.animalId && locationId == testSighting.locationId && date.equals(testSighting.date) && time.equals(testSighting.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, rangerId, animalId, locationId, date, time);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(int animalId) {
+        this.animalId = animalId;
+    }
+
+    public int getRangerId() {
+        return rangerId;
+    }
+
+    public void setRangerId(int rangerId) {
+        this.rangerId = rangerId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
 }
--- src/test/java/models/AnimalSightingTest.java
+++ src/test/java/models/AnimalSightingTest.java
@@ -2,16 +2,45 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalSightingTest {
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception{
+    }
+
+    @Test
+    void GetAndInstantiatesAllObjectsTrueAndCorrectly() {
+        AnimalSighting testSighting = setupSighting();
+        assertTrue(testSighting instanceof AnimalSighting);
+    }
+
+    @Test
+    void GetRangerIdInstantiatesCorrectly_int() {
+        AnimalSighting testSighting = setupSighting();
+        assertEquals(1, testSighting.getRangerId());
+    }
+
+    @Test
+    void GetAnimalWhereaboutsByIdReturnsCorrectly() {
+        AnimalSighting testSighting = setupSighting();
+        assertEquals(1, testSighting.getAnimalId());
+    }
+
+    @Test
+    void GetLocationOfAnimalByIdReturnsCorrectly() {
+        AnimalSighting testSighting = setupSighting();
+        assertEquals(1, testSighting.getLocationId());
+    }
+
+    public AnimalSighting setupSighting() {
+        return new AnimalSighting(1,1,1);
     }
 }
