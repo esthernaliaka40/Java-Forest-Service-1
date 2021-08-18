845e6864c8bb29d763d47caa4da543ccbb114704
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-19 01:05:19 +0300

    add new directories and changes in hbs files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
+import static spark.Spark.*;
+import  java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        get("/", (request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model, "layout.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
