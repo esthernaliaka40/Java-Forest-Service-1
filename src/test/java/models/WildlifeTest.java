758776d2c8b7e70d7f1bfccfa73e8a3de9f5912c
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-18 22:12:11 +0300

    add new class Wildlife an its tests

--- src/test/java/models/WildlifeTest.java
+++ src/test/java/models/WildlifeTest.java
@@ -2,16 +2,45 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WildlifeTest {
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception{
+    }
+
+    @Test
+    void GetObjectsInstantiatesCorrectly_true() {
+        Wildlife testWildlife = setupWildlife();
+        assertTrue(testWildlife instanceof Wildlife);
+    }
+
+    @Test
+    void GetNameOfWildlifeAnimalInstantiatesCorrectly() {
+        Wildlife testWildlife = setupWildlife();
+        assertEquals("Elephant", testWildlife.getName());
+    }
+
+    @Test
+    void GetHealthOfWildlifeAnimalInstantiatesCorrectly() {
+        Wildlife testWildlife = setupWildlife();
+        assertEquals("Okay", testWildlife.getHealth());
+    }
+
+    @Test
+    void GetAgeOfWildlfieAnimalInstantiatesCoreectly_true() {
+        Wildlife testWildlife = setupWildlife();
+        assertEquals("Young", testWildlife.getAge());
+    }
+
+    public Wildlife setupWildlife(){
+        return new Wildlife("Elephant", "Okay", "Young");
     }
 }
