5260caed9014fb94e5ae03264dffa2f80ca5f9be
karuu-01 <brian.muchori@student.moringaschool.com>
2021-08-19 03:02:37 +0300

    add new interfaces and tests

---
+++ src/test/java/Dao/Sql2oAnimalLocationDaoTest.java
@@ -0,0 +1,17 @@
+package Dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oAnimalLocationDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
